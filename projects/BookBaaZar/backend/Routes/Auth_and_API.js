caba2e2bae8e23e85a82a96b930c27643a1543b2
hari Rajak <133476890+Harry1402@users.noreply.github.com>
2025-07-22 22:06:54 +0530

    Adding Register abd Login controller in ym bookbaazar proj

--- projects/BookBaaZar/backend/Routes/Auth_and_API.js
+++ projects/BookBaaZar/backend/Routes/Auth_and_API.js
@@ -1,13 +1,13 @@
 import express from "express";
-import { registerUser } from "../controllers/auth.controllers.js";
+import { registerUser ,loginUser ,AboutMe } from "../controllers/auth.controllers.js";
 
 
 const authRouts = express.Router();
 
 authRouts.post("/auth/register", registerUser)
-// authRouts.post("/auth/login", Login)
+authRouts.post("/auth/login", loginUser)
 // authRouts.post("/auth/api-key", API)
-// authRouts.get("/auth/me", AboutMe)
+authRouts.get("/auth/me", AboutMe)
 
 export default authRouts ;
 
--- projects/BookBaaZar/backend/controllers/auth.controllers.js
+++ projects/BookBaaZar/backend/controllers/auth.controllers.js
@@ -3,12 +3,17 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 // import {User ,API,Book ,Review,Order} from "../models/schema.js";
 import schema from "../models/schema.js";
+const { User, API, Book, Review, Order } = schema;
+import nodemailer from "nodemailer";
 import crypto from "crypto";
- 
+import bcrypt from "bcryptjs";
+import { error } from "console";
+
+
 const registerUser = async (req, res) => {
- 
-  const { name, email, password } = req.body;
-  if (!name || !email || !password) {
+
+  const { name, email, password, username } = req.body;
+  if ( !email || !password || !username) {
     return res.status(400).json({
       message: "All fields are required",
     });
@@ -23,25 +28,27 @@ const registerUser = async (req, res) => {
     }
 
     const user = await User.create({
-      name,
+      username: username,
       email,
       password,
     });
     console.log(user);
 
-    if (!user) {
+    if (!username) {
       return res.status(400).json({
         message: "User not registered",
+
+
       });
     }
 
     const token = crypto.randomBytes(32).toString("hex");
-    console.log(token);
     user.verificationToken = token;
-
     await user.save();
+    
 
-    //send email
+
+    //send email ,basically its called a transporter
     const transporter = nodemailer.createTransport({
       host: process.env.MAILTRAP_HOST,
       port: process.env.MAILTRAP_PORT,
@@ -52,7 +59,9 @@ const registerUser = async (req, res) => {
       },
     });
 
+    // Defining the email options
     const mailOption = {
+      // service: 'Gmail',
       from: process.env.MAILTRAP_SENDEREMAIL,
       to: user.email,
       subject: "Verify your email", // Subject line
@@ -61,19 +70,86 @@ const registerUser = async (req, res) => {
       `,
     };
 
-    await transporter.sendMail(mailOption);
+    await transporter.sendMail(mailOption ,(error, info) => {
+  if (error) {
+    console.log('Error:', error);
+  } else {
+    console.log('Email sent:', info.response);
+  }
+});
 
     res.status(201).json({
       message: "User registered successfully",
       success: true,
     });
+
   } catch (error) {
+    console.error(error);
     res.status(400).json({
       message: "User not registered ",
-      error,
+      error: error.message,
       success: false,
     });
   }
 };
+
+export const loginUser = async (req , res)=>{
+    const {email , password} = req.body;
+
+    try {
+        const user = await User.findOne({email})
+        console.log(user);
+        
+
+        if(!user){
+            return res.status(401).json({
+                error:"User not found"
+            })
+        }
+
+        const isMatch = await password === user.password;
+        console.log(user.password);
+        console.log(password);
+        
+
+        if(!isMatch){
+            return res.status(401).json({
+                error:`Invalid credentials`
+            })
+        }
+
+        const token = jwt.sign({id:user.id} , process.env.JWT_SECRET_KEY,{
+            expiresIn:"7d"
+        })
+
+        res.cookie("jwt" , token , {
+            httpOnly:true,
+            sameSite:"strict",
+            secure:process.env.NODE_ENV !== "development",
+            maxAge:1000 * 60 * 60 * 24 * 7 // 7 days
+        })
+
+        res.status(200).json({
+            success:true,
+            message:"User Logged in successfully",
+            user:{
+                id:user.id,
+                email:user.email,
+                name:user.name,
+                role:user.role,
+                
+            }
+        })
+
+        
+    } catch (error) {
+        console.error("Error creating user:", error);
+        res.status(500).json({
+            error:"Error logging in user"
+        })
+    }
+}
+
+
 
 export { registerUser };
