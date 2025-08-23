e8fed801c4546d7cf1d6f9e9571cfe717d9f3bb2
hari Rajak <133476890+Harry1402@users.noreply.github.com>
2025-08-23 22:09:51 +0530

    Dom

--- javascript/DOM.js
+++ javascript/DOM.js
@@ -4,6 +4,22 @@
 // function ChangeBackgroundWhite() {
 //     document.body.style.backgroundColor = 'White'
 // }
-function ChangeBackgroundColor(color) {
-    document.body.style.backgroundColor = color
-}
+// function ChangeBackgroundColor(color) {
+//     document.body.style.backgroundColor = color
+// }
+
+const ThemeButton = document.getElementById('Theme-button')
+
+ThemeButton.addEventListener("click", () => {
+    // document.body.style.backgroundColor = 'black'
+
+    const Themecolr = document.body.style.backgroundColor;
+
+    if (!Themecolr || Themecolr === "white") {
+        document.body.style.backgroundColor = 'black'
+    }
+    else {
+        document.body.style.backgroundColor = 'white'
+    }
+
+})
