27c839c70d3b6be35b244fb5b5629b6cfb695b62
hari Rajak <133476890+Harry1402@users.noreply.github.com>
2025-08-23 21:45:39 +0530

    Dom

---
+++ javascript/DOM.js
@@ -0,0 +1,9 @@
+// function ChangeBackgroundBlack() {
+//     document.body.style.backgroundColor = 'black'
+// }
+// function ChangeBackgroundWhite() {
+//     document.body.style.backgroundColor = 'White'
+// }
+function ChangeBackgroundColor(color) {
+    document.body.style.backgroundColor = color
+}
