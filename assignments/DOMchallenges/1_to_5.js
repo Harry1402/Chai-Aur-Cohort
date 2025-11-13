3ba222e22833cfda96a775dad2ac3b86116720d1
hari Rajak <133476890+Harry1402@users.noreply.github.com>
2025-11-13 15:04:45 +0530

    Toggle button challenge completed

---
+++ assignments/DOMchallenges/1_to_5.js
@@ -0,0 +1,26 @@
+
+const toogleButton = document.getElementById('toggleButton');
+
+const statuspara = document.getElementById('status');
+
+const bulb = document.getElementById('bulb');
+const body = document.getElementById('body')
+
+toogleButton.addEventListener("click", () => {
+    console.log("inside event");
+
+    if (bulb.classList.contains('off')) {
+        bulb.classList.replace('off', "on");
+        statuspara.textContent = 'Status: ONN';
+        toogleButton.textContent = 'Turn OFF';
+        body.classList.add("dark-mode");
+    } else {
+        bulb.classList.replace('on', 'off');
+        statuspara.textContent = 'Status: OFF';
+        toogleButton.textContent = 'Turn ON';
+        body.classList.remove('dark-mode')
+    }
+
+})
+
+// function to toggle btw dark and light mode 
