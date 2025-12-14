0aa41c49838087f1a9ce65a10e565c64f1fec324
hari Rajak <133476890+Harry1402@users.noreply.github.com>
2025-12-14 13:05:28 +0530

    Understanding Useeffect

---
+++ React/useeffect/UseEffect/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [quote, setQuote] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.freeapi.app/api/v1/public/quotes/quote/random")
+      .then((response) => response.json())
+      .then((result) => {
+        setQuote(result.data); // storing the full object
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <>
+      {quote && (
+        <>
+          <p>"{quote.content}"</p>
+          <p>— {quote.author}</p>
+        </>
+      )}
+    </>
+  );
+}
+
+export default App;
